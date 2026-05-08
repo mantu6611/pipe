fe0e7a60d64bb923148469af723c5d5487823b91
Ubuntu <ubuntu@ip-172-31-82-225.ec2.internal>
2026-05-08 13:23:31 +0000

    fhbhg

--- test.js
+++ test.js
@@ -1,32 +1,9 @@
+const assert = require('assert');
 
-const { requestHandler } = require("./index");
+describe('Sample Test', function () {
 
-function mockResponse() {
-  return {
-    statusCode: null,
-    headers: null,
-    body: "",
-    writeHead(status, headers) {
-      this.statusCode = status;
-      this.headers = headers;
-    },
-    end(text) {
-      this.body = text;
-    }
-  };
-}
+    it('should pass', function () {
+        assert.equal(1, 1);
+    });
 
-function testServer() {
-  const req = {};
-  const res = mockResponse();
-
-  requestHandler(req, res);
-
-  if (res.statusCode === 200 && res.body === "Hello from Node.js server!") {
-    console.log("✅ Test passed");
-  } else {
-    console.log("❌ Test failed");
-  }
-}
-
-testServer();
+});
