9ad9d97fe78b215cc432f147712fb97ca31c0b52
Ubuntu <ubuntu@ip-172-31-82-225.ec2.internal>
2026-05-09 08:42:33 +0000

    jhjhhh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("maksood");
+  res.end("chapri");
 }
 
 const server = http.createServer(requestHandler);
