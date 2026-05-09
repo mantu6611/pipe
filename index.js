0f523c8aebcaa889b38af0acc43c434e47b1b536
Ubuntu <ubuntu@ip-172-31-82-225.ec2.internal>
2026-05-09 08:39:48 +0000

    jhjhs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("sonar this  d");
+  res.end("maksood");
 }
 
 const server = http.createServer(requestHandler);
